8274178f11c9a6c8567167860efc86b2e9adce95
Kevin Putnam <kevin.putnam@intel.com>
2022-12-05 20:51:03 -0800

    removed unneeded theme
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

--- conf.py
+++ conf.py
@@ -62,7 +62,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','doxyrest-sphinx']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
